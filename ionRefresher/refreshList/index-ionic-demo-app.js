16b43b10c6bc69989a3d37f62b88d497d79fa589
Ionitron <hi@ionicframework.com>
2014-08-06 21:49:24 +0000

    demos: update for 1.0.0-beta.11

--- ionRefresher/refreshList/index-ionic-demo-app.js
+++ ionRefresher/refreshList/index-ionic-demo-app.js
@@ -84,8 +84,8 @@ angular.module('refreshList'
            '<head>',
            '  <meta charset="utf-8">',
            '  <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width">',
-           '  <link rel="stylesheet" href="http://code.ionicframework.com/1.0.0-beta.10-nightly-16570/css/ionic.css">',
-           '  <script src="http://code.ionicframework.com/1.0.0-beta.10-nightly-16570/js/ionic.bundle.js"></script>',
+           '  <link rel="stylesheet" href="http://code.ionicframework.com/1.0.0-beta.11/css/ionic.css">',
+           '  <script src="http://code.ionicframework.com/1.0.0-beta.11/js/ionic.bundle.js"></script>',
            '</head>',
            '<body>',
            (demo.files.html || []).map(function(file) {
